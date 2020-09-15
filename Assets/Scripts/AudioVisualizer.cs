f05894451142868dccd5549cb115a841c3a515d6
Howard Tseng <howardt12345@gmail.com>
2020-09-15 11:16:47 -0400

    Added a lot of stuff
    - Added toggle between a single ring and multiple rings
    - Added setting hue shift parameters

--- Assets/Scripts/AudioVisualizer.cs
+++ Assets/Scripts/AudioVisualizer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Serialization;
+using MyBox;
 
 [Serializable]
 public class Ring
@@ -10,9 +11,7 @@ public class Ring
     public GameObject parent;
     public GameObject[] cubes;
 
-    public Ring()
-    {
-    }
+    public Ring() { }
 
     public Ring(GameObject parent, GameObject[] cubes)
     {
@@ -21,42 +20,68 @@ public class Ring
     }
 }
 
+public enum RingState 
+{
+    Single,
+    Multiple
+}
+public enum ColorState
+{
+    None,
+    Hue, 
+    Saturation
+}
+
 public class AudioVisualizer : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject parent;
-    public int numberOfRings = 1;
     public AudioSource src;
+    public RingState ringState = RingState.Single;
+    [ConditionalField("ringState", false, RingState.Single)] 
+    public float radius = 15f;
+    [ConditionalField("ringState", false, RingState.Multiple), Min(2)] 
+    public int numberOfRings = 2;
+    [ConditionalField("ringState", false, RingState.Multiple)] 
     public float maxRadius = 20f;
+    [ConditionalField("ringState", false, RingState.Multiple)] 
     public float minRadius = 10f;
+    
     public float yScale = 1f;
     public float xScale = 1f;
     public int range = 1024;
     public float ringRotateSpeed;
     public float rotateSpeed;
-    public bool changeColor;
     public bool topOnly;
+    public ColorState changeColor;
+
+    [ConditionalField("changeColor", false, ColorState.Hue), Min(0)]
+    public float startingHue = 0.67f;
+
+    [ConditionalField("changeColor", false, ColorState.Hue)]
+    public float shiftFactor = 4f;
 
     public Ring[] rings;
 
     private void Start ()
     {
-        rings = new Ring[numberOfRings];
-        for (int i = 0; i < numberOfRings; i++)
+        rings = new Ring[ringState == RingState.Single ? 1 : numberOfRings];
+        for (int i = 0; i < rings.Length; i++)
         {
             rings[i] = new Ring
             {
                 parent = new GameObject(),
-                cubes = new GameObject[Convert.ToInt32(
-                    Mathf.PI * (maxRadius - ((maxRadius - minRadius) *
-                                             Convert.ToSingle(Mathf.Log(1f + (i) / (numberOfRings - 1f), 2)))) *
-                    xScale)]
+                cubes = new GameObject[ringState == RingState.Single 
+                    ? Convert.ToInt32(Mathf.PI * radius * xScale)
+                    : Convert.ToInt32(Mathf.PI * (maxRadius - ((maxRadius - minRadius) * Convert.ToSingle(Mathf.Log(1f + (i) / (numberOfRings - 1f), 2)))) * xScale)
+                ]
             };
             rings[i].parent.transform.parent = parent.transform;
-            for (int j = 0; j < rings[i].cubes.Length; j++) {
+            for (int j = 0; j < rings[i].cubes.Length; j++) 
+            {
                 float angle = j * Mathf.PI * 2 / rings[i].cubes.Length;
-                Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) *
-                              (maxRadius - ((maxRadius - minRadius) * Convert.ToSingle(Mathf.Log(1f + (i)/(numberOfRings-1f), 2))));
+                Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) 
+                    * (ringState == RingState.Single ? radius : (maxRadius - ((maxRadius - minRadius) * Convert.ToSingle(Mathf.Log(1f + (i)/(numberOfRings-1f), 2)))));
                 GameObject tmp = Instantiate(prefab, pos, Quaternion.identity);
                 tmp.transform.parent = rings[i].parent.transform;
                 rings[i].cubes[j] = tmp;
@@ -64,22 +89,29 @@ public class AudioVisualizer : MonoBehaviour
         }
     }
 
-    private void Update () {
+    private void Update () 
+    {
         float[] spectrum = new float[range];
         src.GetSpectrumData (spectrum, 0, FFTWindow.Hanning);
-        for (int i = 0; i < numberOfRings; i++) {
-            for (int j = 0; j < rings[i].cubes.Length; j++) {
+        for (int i = 0; i < rings.Length; i++) 
+        {
+            for (int j = 0; j < rings[i].cubes.Length; j++) 
+            {
                 rings[i].cubes[j].transform.localScale = new Vector3(rings[i].cubes[j].transform.localScale.x, spectrum[(rings[i].cubes.Length*i) + j] * yScale * (topOnly ? 25 : 50), rings[i].cubes[j].transform.localScale.z);;
                 rings[i].cubes[j].transform.position = new Vector3(rings[i].cubes[j].transform.position.x, topOnly ? (spectrum[(rings[i].cubes.Length*i) + j] * yScale) * 12.5f : 0, rings[i].cubes[j].transform.position.z);
-                if (changeColor) {
-                    rings[i].cubes[j].GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(0.67f-(spectrum[(rings[i].cubes.Length*i) + j]*4f), 1, 1));
+                if (changeColor == ColorState.Hue)
+                {
+                    startingHue = Mathf.Clamp(startingHue, 0f, 1f);
+                    rings[i].cubes[j].GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(Mathf.Clamp(startingHue-(spectrum[(rings[i].cubes.Length*i) + j]*shiftFactor), 0f, 1f), 1, 1));
                 }
             }
         }
 
-        for (int i = 0; i < numberOfRings; i++) {
+        for (int i = 0; i < rings.Length; i++) 
+        {
             rings[i].parent.transform.Rotate(0, (i % 2 == 0 ? ringRotateSpeed : -ringRotateSpeed), 0);
-            for (int j = 0; j < rings[i].cubes.Length; j++) {
+            for (int j = 0; j < rings[i].cubes.Length; j++) 
+            {
                 rings[i].cubes[j].transform.Rotate (0, (i % 2 == 0 ? rotateSpeed : -rotateSpeed), 0);		
             }
         }
